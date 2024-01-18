6825a0f012e97ce3b53d5833815f0cfc44b677dc
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2024-01-18 12:07:20 +0000

    Update SecurityConfiguration

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.makersacademy.acebook;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,8 +27,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/posts").hasRole("USER")
+                .antMatchers("/posts/**").hasRole("USER")
                 .antMatchers("/users").permitAll()
+                .anyRequest().authenticated()
                 .and().formLogin();
     }
 
